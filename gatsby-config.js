2a4c52fd42122329f2e0433720c093eebe67e26a
Mark Crisostomo <mcrisostomo9@gmail.com>
2020-02-28 11:43:47 -0800

    commented out manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,23 +15,23 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    // {
+    //   resolve: `gatsby-plugin-manifest`,
+    //   options: {
+    //     name: `gatsby-starter-default`,
+    //     short_name: `starter`,
+    //     start_url: `/`,
+    //     background_color: `#663399`,
+    //     theme_color: `#663399`,
+    //     display: `minimal-ui`,
+    //     icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+    //   },
+    // },
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: "gatsby-source-sanity",
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    {
-      resolve: 'gatsby-source-sanity',
-      options: {
-        projectId: 'cdmdcreu',
-        dataset: 'production',
+        projectId: "cdmdcreu",
+        dataset: "production",
         // a token with read permissions is required
         // if you have a private dataset
         // token: process.env.MY_SANITY_TOKEN,
@@ -44,11 +44,11 @@ module.exports = {
         fonts: [
           {
             family: `Poppins`,
-            variants: [`400`,`600`,`700`]
+            variants: [`400`, `600`, `700`],
           },
         ],
       },
-    }
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
