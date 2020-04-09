6602cf82cea196b54ea315088ef17c8cd2179af4
Mark Crisostomo <mcrisostomo9@gmail.com>
2020-04-09 14:56:06 -0700

    testing functions

--- .netlify/functions/subscribe.js
+++ .netlify/functions/subscribe.js
@@ -1,4 +1,21 @@
-exports.handler = (event, context, callback) => {
+const Hubspot = require("hubspot")
+const fetch = require("node-fetch")
+
+exports.handler = async (event, context, callback) => {
+  const hubspot = new Hubspot({
+    apiKey: process.env.HUBSPOT_API_KEY,
+  })
+
+  const which = await hubspot.forms.getById("7448833")
+  const formm = await hubspot.forms.getById(
+    "117cadbe-8565-4c1d-b3fc-f6cfe9263b19"
+  )
+
+  console.log(which)
+  console.log(formm)
+
+  // hubspot.forms.submit("7448833", "117cadbe-8565-4c1d-b3fc-f6cfe9263b19", data)
+
   callback(null, {
     statusCode: 200,
     body: "No worries, all is working fine!",
