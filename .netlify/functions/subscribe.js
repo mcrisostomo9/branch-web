1e7bbea4444224cf9aa2d24c2ca000c73728cf02
Mark Crisostomo <mcrisostomo9@gmail.com>
2020-04-09 15:03:44 -0700

    testing submit endpoint

--- .netlify/functions/subscribe.js
+++ .netlify/functions/subscribe.js
@@ -6,15 +6,44 @@ exports.handler = async (event, context, callback) => {
     apiKey: process.env.HUBSPOT_API_KEY,
   })
 
-  const which = await hubspot.forms.getById("7448833")
-  const formm = await hubspot.forms.getById(
-    "117cadbe-8565-4c1d-b3fc-f6cfe9263b19"
-  )
+  var data = {
+    submittedAt: "1517927174000",
+    fields: [
+      {
+        name: "email",
+        value: "example@example.com",
+      },
+      {
+        name: "firstname",
+        value: "Jeff",
+      },
+    ],
+    context: {
+      hutk: ":hutk", // include this parameter and set it to the hubspotutk cookie value to enable cookie tracking on your submission
+      pageUri: "www.example.com/page",
+      pageName: "Example page",
+    },
+    legalConsentOptions: {
+      // Include this object when GDPR options are enabled
+      consent: {
+        consentToProcess: true,
+        text:
+          "I agree to allow Example Company to store and process my personal data.",
+        communications: [
+          {
+            value: true,
+            subscriptionTypeId: 999,
+            text:
+              "I agree to receive marketing communications from Example Company.",
+          },
+        ],
+      },
+    },
+  }
 
-  console.log(which)
-  console.log(formm)
-
-  // hubspot.forms.submit("7448833", "117cadbe-8565-4c1d-b3fc-f6cfe9263b19", data)
+  hubspot.forms
+    .submit("7448833", "117cadbe-8565-4c1d-b3fc-f6cfe9263b19", data)
+    .then(res => console.log(res))
 
   callback(null, {
     statusCode: 200,
