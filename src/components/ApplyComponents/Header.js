9111876bcd6c91f1c1d9d83219c9a8bdf6250fc1
Mark Crisostomo <mcrisostomo9@gmail.com>
2020-04-20 10:53:52 -0700

    fixed apply header for mobile

--- src/components/ApplyComponents/Header.js
+++ src/components/ApplyComponents/Header.js
@@ -3,22 +3,38 @@ import styled from "styled-components"
 import { Link } from "gatsby"
 import Logo from "../Shared/Logo"
 import Container from "../Shared/Container"
+import { mq } from "../../utils/styles"
 
 const Nav = styled(Container)`
   position: relative;
+  display: flex;
+  flex-direction: column;
 `
 
 const HomeLink = styled.div`
+  order: 2;
+  text-align: center;
+  margin-top: 1rem;
   a {
     color: var(--pink-theme);
+  }
+
+  @media (min-width: ${mq.m768}) {
+    text-align: left;
+    margin-top: 0rem;
   }
 `
 
 const StyledLogo = styled(Logo)`
-  position: absolute;
-  left: 50%;
-  top: 50%;
-  transform: translate(-50%, -50%);
+  order: 1;
+  align-self: center;
+
+  @media (min-width: ${mq.m768}) {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+  }
 `
 
 const Header = () => {
